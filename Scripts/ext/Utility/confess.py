b7012f8558bc80f993118d9ac74b2c88644096bc
Darkyl <darkprankingyt@gmail.com>
2024-06-19 14:39:42 +0200

    Added Confessions

---
+++ Scripts/ext/Utility/confess.py
@@ -0,0 +1,88 @@
+import os
+import json
+
+import lightbulb
+import hikari
+
+import bot_utils as utils
+import config_reader as config
+import image_manager
+import buttons
+
+plugin = lightbulb.Plugin("Confess", "Confess anonymously")
+
+@plugin.command
+@lightbulb.add_cooldown(3, 3, lightbulb.UserBucket)
+@lightbulb.option("text", "Your confession", str, required=True)
+@lightbulb.option("image", "An image to attach", hikari.OptionType.ATTACHMENT, required=False, default=None)
+@lightbulb.command("confess", "Confess anonysmously", pass_options=True)
+@lightbulb.implements(lightbulb.SlashCommand)
+async def confess_command(ctx: lightbulb.SlashContext, text: str, image: hikari.File):
+    if ctx.author.is_bot or ctx.author.is_system:
+        await ctx.respond("This command can not be executed by other bots.", flags=hikari.MessageFlag.EPHEMERAL)
+        return
+    
+    id = await utils.generate_id()
+
+    image_path = None
+
+    if image:
+        supported_formats = ["jpg", "jpeg", "JPG", "JPEG", "png", "PNG", "gif", "gifv"]
+
+        filetype = str(image.filename).split(".")[-1]
+
+        if not filetype in supported_formats:
+            txt = ""
+            for format in supported_formats:
+                txt += f"\n.{format}"
+
+            await ctx.respond(f"Discord only supports the following filetypes:{txt}", flags=hikari.MessageFlag.EPHEMERAL)
+            return
+
+        image_path = await image_manager.download_image(image.url, id, os.path.join(config.Paths.data_folder, "Confession Images"))
+
+        if image_path == None:
+            await ctx.respond("There was an error while attempting to download your image. Maybe it's in the wrong file format?", flags=hikari.MessageFlag.EPHEMERAL)
+            return
+    
+    if not id:
+        await ctx.respond(f"An error occurred. {await utils.error_fun}", flags=hikari.MessageFlag.EPHEMERAL)
+        return
+    
+    embed = hikari.Embed(title=f"Confession {id}", description=text, color=hikari.Color.from_hex_code("#36ff6b"))
+
+    if image:
+        embed.set_image(image)
+    
+    embed.set_author(name="An anonymous confession")
+
+    view = buttons.Confess()
+
+    message = await plugin.app.rest.create_message(channel=config.Channels.confession_approving_channel, embed=embed, components=view)
+
+    db_path = os.path.join(config.Paths.data_folder, "Database", "confessions.json")
+
+    if os.path.exists(db_path):
+        try:
+            with open(db_path, "r") as file:
+                confessions = json.load(file)
+        except json.JSONDecodeError:
+            confessions = {}
+    else:
+        confessions = {}
+
+    if image_path == None:
+        image_path = "None"
+
+    confessions[id] = [text, message.id, str(image_path)]
+
+    with open(db_path, "w") as file:
+        json.dump(confessions, file, indent=4)
+    
+    await ctx.respond(f"Your confession has been submitted with the ID *{id}*. A moderator will either approve or deny the confession. This may take a little time.", flags=hikari.MessageFlag.EPHEMERAL)
+
+def load(bot):
+    bot.add_plugin(plugin)
+
+def unload(bot):
+    bot.remove(plugin)

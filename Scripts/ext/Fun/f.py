efea477c7ae83e67d553cfc39a95ebeeaa437c61
Darkyl <darkprankingyt@gmail.com>
2024-06-22 22:28:45 +0200

    Update f.py

--- Scripts/ext/Fun/f.py
+++ Scripts/ext/Fun/f.py
@@ -8,6 +8,7 @@ import config_reader as config
 
 plugin = lightbulb.Plugin("F", "Pay your respects!")
 
+hearts = ['❤', '💛', '💚', '💙', '💜', '♥', 'o7']
 
 @plugin.command
 @lightbulb.add_cooldown(3, 3, lightbulb.UserBucket)
@@ -20,27 +21,27 @@ async def f_command(ctx: lightbulb.SlashContext, text: str):
 
     reason = f"for **'{text}'** " if text else ""
 
-    hearts = ['❤', '💛', '💚', '💙', '💜', '♥']
-
     await ctx.respond(f"**{ctx.author.mention}** has paid their respect {reason}{random.choice(hearts)}")
 
-#@plugin.command
-#@lightbulb.add_cooldown(3, 3, lightbulb.UserBucket)
-#@lightbulb.command("respect", "Press F to pay respect.")
-#@lightbulb.implements(lightbulb.MessageCommand)
-#async def f_message_command(ctx: lightbulb.Context):
-#    if ctx.author.is_bot or ctx.author.is_system:
-#        return
-#    
-#    
-#    logger.info(f"{ctx.author.username} used respect message command.")#
-#
-#    message = ctx.options.target
-#    author = message.author#
-#
-#    hearts = ['❤', '💛', '💚', '💙', '💜', '♥']#
-#
-#    await ctx.app.rest.create_message(f"**{ctx.author.mention}** has paid their respect for **{author.mention}**{random.choice(hearts)}", reply=message.id, mentions_reply=True)
+@plugin.command
+@lightbulb.add_cooldown(3, 3, lightbulb.UserBucket)
+@lightbulb.command("respect", "Press F to pay respect.")
+@lightbulb.implements(lightbulb.MessageCommand)
+async def f_message_command(ctx: lightbulb.Context):
+    if ctx.author.is_bot or ctx.author.is_system:
+        return
+
+    from bot import logger
+    logger.info(f"{ctx.author.username} used respect message command.")#
+
+    message = ctx.options.target
+    author = message.author#
+
+    msg = await plugin.app.rest.fetch_message(ctx.channel_id, message)
+
+    await msg.respond(f"**{ctx.author.mention}** has paid their respect for {author.mention}{random.choice(hearts)}", reply=msg)
+
+    await ctx.respond("Respects paid o7", flags=hikari.MessageFlag.EPHEMERAL)
 
 
 def load(bot):
